035cb77b429aaf96d034cb92c728efa03628dbc2
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2021-11-12 09:05:35 -0800

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -9,7 +9,8 @@ function getURL() {
             getUser(searches.slice(5,poskey), "8688c07f-e1c2-4f47-ae37-01013664ce6f");
         }
     } else {
-        document.getElementById("statspage").style.display = "none";
+        document.getElementById("main").innerHTML = 
+    		"<div id=\"mainpage\" class=\"mainpage\"><form><p>Show Skyblock Stats for:</p><input name=\"ign\" type=\"search\" placeholder=\"Enter username\" pattern=\"\w{1,}\" autofocus required><p>Your API key:</p><input name=\"key\" type=\"search\" placeholder=\"Enter your API key\" autofocus><br><button type=\"submit\">Show stats</button></form></div>";
     }
 }
 
