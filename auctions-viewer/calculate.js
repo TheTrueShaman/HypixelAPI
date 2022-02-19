8be6de7b1063806163a15c923152277ecadb6f5c
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-02-19 13:33:48 -0800

    Update calculate.js

--- auctions-viewer/calculate.js
+++ auctions-viewer/calculate.js
@@ -2,4 +2,5 @@
 Note to future Shaman:
 Auctions do not need API keys, unless you are searching for auctions by a specific player. 
 Implement both, but prioritize the normal one rather than the player auctions search. Make sure both fit on the page and make sense.
+Put the first in auctions-viewer, put the second in player-auctions-viewer.
 */
