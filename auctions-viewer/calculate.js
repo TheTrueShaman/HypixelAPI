ee1741be878264fff1d140f486c6456a289bd4e9
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-02-20 13:26:53 -0800

    Update calculate.js

--- auctions-viewer/calculate.js
+++ auctions-viewer/calculate.js
@@ -4,3 +4,12 @@ Auctions do not need API keys, unless you are searching for auctions by a specif
 Implement both, but prioritize the normal one rather than the player auctions search. Make sure both fit on the page and make sense.
 Put the first in auctions-viewer, put the second in player-auctions-viewer.
 */
+
+async function getPage(page) {
+	let auctionsURL = new URL('https://api.hypixel.net/skyblock/profiles')
+	auctionsURL.searchParams.set('page', page);
+	const auctionPageData = await fetch(auctionsURL);
+	console.log(auctionPageData);
+	const auctionPageJSON = await auctionPageData.json();
+	console.log(auctionPageJSON);
+}
