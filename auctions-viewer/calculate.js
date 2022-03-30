e9dfebce9ad02ace2ff2e1d41d7e07b7f3a4cc54
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-03-30 14:08:08 -0700

    Replacing spaces with tabs

--- auctions-viewer/calculate.js
+++ auctions-viewer/calculate.js
@@ -6,45 +6,45 @@ Put the first in auctions-viewer, put the second in player-auctions-viewer.
 */
 
 function loadOne() {
-    document.getElementById("loadone").innerHTML = "Reload One";
-    document.getElementById("loadall").innerHTML = "Load All";
-    getAuctions(0);
+	document.getElementById("loadone").innerHTML = "Reload One";
+	document.getElementById("loadall").innerHTML = "Load All";
+	getAuctions(0);
 }
 
 function loadAll() {
-    document.getElementById("loadone").innerHTML = "Load One";
-    document.getElementById("loadall").innerHTML = "Reload All";
-    getAuctions(1);
+	document.getElementById("loadone").innerHTML = "Load One";
+	document.getElementById("loadall").innerHTML = "Reload All";
+	getAuctions(1);
 }
 
 async function getAuctions(type) {
-    window.SB_Item_List = [];
-    window.SB_Item_List.push(await getPage(0));
-    if (type == 1) {
-        let pages = window.SB_Item_List[0]['totalPages']
-        for (let i = 1; i < pages; i++) {
-            window.SB_Item_List.push(await getPage(i));
-        }
-    }
+	window.SB_Item_List = [];
+	window.SB_Item_List.push(await getPage(0));
+	if (type == 1) {
+		let pages = window.SB_Item_List[0]['totalPages']
+		for (let i = 1; i < pages; i++) {
+			window.SB_Item_List.push(await getPage(i));
+		}
+	}
 }
 
 async function getPage(page) {
-    let auctionsURL = new URL('https://api.hypixel.net/skyblock/auctions')
-    auctionsURL.searchParams.set('page', page);
-    const auctionPageData = await fetch(auctionsURL);
-    const auctionPageJSON = await auctionPageData.json();
-    console.log(auctionPageJSON);
-    return auctionPageJSON;
+	let auctionsURL = new URL('https://api.hypixel.net/skyblock/auctions')
+	auctionsURL.searchParams.set('page', page);
+	const auctionPageData = await fetch(auctionsURL);
+	const auctionPageJSON = await auctionPageData.json();
+	console.log(auctionPageJSON);
+	return auctionPageJSON;
 }
 
 function settings(setting) {
-    const id = "option" + setting.toString();
-    window.search_settings[setting-1] = !window.search_settings[setting-1];
-    if (window.search_settings[setting-1] == false) {
-        document.getElementById(id).innerHTML = "Off"
-    } else {
-        document.getElementById(id).innerHTML = "On"
-    }
+	const id = "option" + setting.toString();
+	window.search_settings[setting-1] = !window.search_settings[setting-1];
+	if (window.search_settings[setting-1] == false) {
+		document.getElementById(id).innerHTML = "Off"
+	} else {
+		document.getElementById(id).innerHTML = "On"
+	}
 }
 
 function search() {
@@ -94,16 +94,15 @@ function search_map_title_lore(element, index) {
 }
 
 function display_results(results) {
-    if (results.length != 0) {
-        document.getElementById('main').innerHTML = '<div id="itemview"></div><div id="inventoryview"></div><div class="selection" style="width: calc(calc(9 * calc(var(--inv-size) + calc(var(--inv-size)/8))) + calc(var(--inv-size)/2))"><button id="previous" onclick="previous()">Not Possible</button><button id="next" onclick="next()">Not Possible</button></div>';
-        let results_amount = 0;
-        let better_results = [];
-        for (let a = 0; a < results.length; a++) {
-            results_amount += results[a].length;
-            better_results = better_results.concat(results[a]);
-        }
-		console.log(results_amount);
-        console.log(better_results);	
+	if (results.length != 0) {
+		document.getElementById('main').innerHTML = '<div id="itemview"></div><div id="inventoryview"></div><div class="selection" style="width: calc(calc(9 * calc(var(--inv-size) + calc(var(--inv-size)/8))) + calc(var(--inv-size)/2))"><button id="previous" onclick="previous()">Not Possible</button><button id="next" onclick="next()">Not Possible</button></div>';
+		let results_amount = 0;
+		let better_results = [];
+		for (let a = 0; a < results.length; a++) {
+			results_amount += results[a].length;
+			better_results = better_results.concat(results[a]);
+		}
+		console.log(better_results);	
 		window.index = 0;
 		window.items = []
 		for (let i = 0; i < better_results.length; i++) {
@@ -113,7 +112,7 @@ function display_results(results) {
 			document.getElementById('inventoryview').innerHTML = 'Next';
 		}
 		draw_inventory();
-    }
+	}
 }
 
 function draw_inventory() {
