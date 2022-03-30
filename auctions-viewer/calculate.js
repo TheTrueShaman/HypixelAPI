a65ad9f13ae58f55fc23d99f019722c03f9fb482
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-03-30 13:59:37 -0700

    Update calculate.js

--- auctions-viewer/calculate.js
+++ auctions-viewer/calculate.js
@@ -95,32 +95,42 @@ function search_map_title_lore(element, index) {
 
 function display_results(results) {
     if (results.length != 0) {
-        document.getElementById('main').innerHTML = '<div id="itemview"></div><div id="inventoryview"></div>';
+        document.getElementById('main').innerHTML = '<div id="itemview"></div><div id="inventoryview"></div><div class="selection" style="width: calc(calc(9 * calc(var(--inv-size) + calc(var(--inv-size)/8))) + calc(var(--inv-size)/2))"><button id="previous" onclick="previous()">Not Possible</button><button id="next" onclick="next()">Not Possible</button></div>';
         let results_amount = 0;
         let better_results = [];
         for (let a = 0; a < results.length; a++) {
             results_amount += results[a].length;
             better_results = better_results.concat(results[a]);
         }
-        console.log(better_results);
-        if (results_amount % 9 != 0) {
-            results_amount += (9 - (results_amount % 9));
-        }
-        for (let i = 0; i < results_amount; i++) {
-    		document.getElementById('inventoryview').innerHTML = document.getElementById('inventoryview').innerHTML + '<div class="inventoryslot"></div>';
-        }
+		console.log(results_amount);
+        console.log(better_results);	
+		window.index = 0;
 		window.items = []
 		for (let i = 0; i < better_results.length; i++) {
 			window.items.push(window.SB_Item_List[better_results[i][0]]['auctions'][better_results[i][1]]);
 		}
-        /*
-        for (let i = 0; i < better_results.length; i++) {
-		
-            let command = "write_slot(" + JSON.stringify(json_inventory[i]) + ")"; This line needs to be rewritten.
-			document.getElementsByClassName("inventoryslot")[27+i].setAttribute("onmouseover", command);
-			document.getElementsByClassName("inventoryslot")[27+i].setAttribute("onmouseout", "document.getElementById('itemview').innerHTML = '';");
-			document.getElementsByClassName("inventoryslot")[27+i].innerHTML = draw_slot(json_inventory[i]);
-        }
-        */
+		if (window.items.length > 54) {
+			document.getElementById('inventoryview').innerHTML = 'Next';
+		}
+		draw_inventory();
     }
+}
+
+function draw_inventory() {
+	let results_amount= window.items.length;
+	if (results_amount % 9 != 0) {
+		results_amount += (9 - (results_amount % 9));
+	}
+	for (let i = 0; i < Math.min((results_amount - window.index), 54); i++) {
+		document.getElementById('inventoryview').innerHTML = document.getElementById('inventoryview').innerHTML + '<div class="inventoryslot"></div>';
+	}
+		
+	/*
+	for (let i = window.index; i < Math.min((better_results.length - window.index), 54 + window.index); i++) {
+		let command = "write_slot(" + JSON.stringify(json_inventory[i]) + ")"; This line needs to be rewritten.
+		document.getElementsByClassName("inventoryslot")[27+i].setAttribute("onmouseover", command);
+		document.getElementsByClassName("inventoryslot")[27+i].setAttribute("onmouseout", "document.getElementById('itemview').innerHTML = '';");
+		document.getElementsByClassName("inventoryslot")[27+i].innerHTML = draw_slot(json_inventory[i]);
+	}
+	*/
 }
