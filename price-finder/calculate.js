04718a601c971d283829529bf483c6fa96c376b5
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2024-05-08 19:22:25 -0700

    Update calculate.js

--- price-finder/calculate.js
+++ price-finder/calculate.js
@@ -1,1 +1,21 @@
-//https://moulberry.codes/lowestbin.json
+async function getAuctions(type) {
+	window.SB_Item_List = [];
+	window.SB_Item_List.push(await getAuctionsPage(0));
+	let pages = window.SB_Item_List[0]['totalPages']
+	for (let i = 1; i < pages; i++) {
+		window.SB_Item_List.push(await getAuctionsPage(i));
+	}
+}
+
+async function getAuctionsPage(page) {
+	let auctionsURL = new URL('https://api.hypixel.net/v2/skyblock/auctions')
+	auctionsURL.searchParams.set('page', page);
+	const auctionPageData = await fetch(auctionsURL);
+	const auctionPageJSON = await auctionPageData.json();
+	console.log(auctionPageJSON);
+	return auctionPageJSON;
+}
+
+async function getBazaar() {
+  //https://api.hypixel.net/v2/skyblock/bazaar
+}
